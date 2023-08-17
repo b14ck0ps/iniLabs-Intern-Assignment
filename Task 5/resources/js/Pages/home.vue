b068cff8a6adaaafe8a9b6262c6c3c506863bc0e
Azran Hossain <mr69751@gmail.com>
2023-08-17 11:18:35 +0600

    responsive card for tasks

--- Task 5/resources/js/Pages/home.vue
+++ Task 5/resources/js/Pages/home.vue
@@ -34,27 +34,29 @@ function deleteTodo(id) {
 <template>
     <Head title="TODOS" />
     <main class="flex flex-col items-center justify-center mt-32">
-        <h1 class="mb-10 text-3xl">TODO APP</h1>
-        <form class="flex gap-5" @submit.prevent="submit">
+        <div class="mb-10 text-center">
+            <h1 class="text-4xl hover:cursor-pointer ">TODO APP</h1>
+            <p class="text-xs">Made By Azran</p>
+        </div>
+        <form class="flex gap-5 mb-10" @submit.prevent="submit">
             <input id="todo" type="text" placeholder="Add new Todo" v-model="form.task"
-                className="input input-bordered w-[400px] max-w-xs focus:outline-none" />
-            <button className="btn btn-outline btn-primary">Add</button>
+                className="input input-bordered w-[300px] md:w-[400px] max-w-xs focus:outline-none" />
+            <button className="btn btn-outline btn-neutral">Add</button>
         </form>
 
-        <section class="w-[400px]">
-            <ul class="mt-10">
-                <li v-for="(todo, index) in todos" :key="index"
-                    class="flex items-center justify-between w-full p-5 mb-5 rounded-md shadow bg-slate-100 ">
-                    <div class="flex items-center gap-2">
-                        <input @click="updateTodo(todo)" :checked="todo.completed" type="checkbox"
-                            class="checkbox checkbox-primary checkbox-xs" />
-                        <span class="text-xl font-bold" :class="{ 'line-through text-gray-300': todo.completed }"
-                            v-text="todo.task"></span>
+        <section class="xl:w-[600px] w-[350px] md:w-[400px]">
+            <div v-for="(todo, index) in todos" :key="index">
+                <div class="p-5 mb-10 overflow-hidden rounded-md shadow bg-slate-100">
+                    <div class="flex justify-between">
+                        <div class="flex gap-4">
+                            <input @click="updateTodo(todo)" :checked="todo.completed" type="checkbox" class="checkbox " />
+                            <p class="text-xl text-gray-700" :class="{ 'line-through': todo.completed }">{{
+                                todo.task }}</p>
+                        </div>
+                        <button @click="deleteTodo(todo.id)" class="btn btn-outline btn-error btn-sm">Delete</button>
                     </div>
-                    <button @click="deleteTodo(todo.id)" class="btn btn-outline btn-error btn-sm">Delete</button>
-                </li>
-            </ul>
+                </div>
+            </div>
         </section>
-
     </main>
 </template>
